76cb09cef3309254140cf02355bf4479ac808b39
odnnsk <oleynikov88@mail.ru>
2018-04-30 03:58:01 +0700

    fix

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,5 +4,6 @@ module.exports = function(){
     $.gulp.task('watch', function(){
         $.gulp.watch('./source/style/*.scss', $.gulp.series('sass'));
         $.gulp.watch('./source/views/pages/*.pug', $.gulp.series('pug'));
+        $.gulp.watch('./source/js/*.js', $.gulp.series('js:process'));
     });
 };
